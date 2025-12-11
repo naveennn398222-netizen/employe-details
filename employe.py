6ef3591a8d4c5aa5bcad981acec6981dee35bd73
naveennn398222-netizen <naveennn398222@gmail.com>
2025-12-11 14:25:18 +0530

    save

---
+++ employe.py
@@ -0,0 +1,14 @@
+def employee_details(name, emp_id, department, salary):
+    result = (
+        f"Employee Name: {name}\n"
+        f"Employee ID: {emp_id}\n"
+        f"Department  : {department}\n"
+        f"Salary      : {salary}"
+    )
+    return result
+if __name__ == "__main__":
+    name = "furkhan"
+    emp_id = "E1001"
+    department = "kitchen"
+    salary = 250
+    print(employee_details(name, emp_id, department, salary))
