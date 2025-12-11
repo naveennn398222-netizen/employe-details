081f7b7d09f5062302e610d7e6844314580f038a
naveen <naveennn398222@gmail.com>
2025-12-11 15:39:49 +0530

    Update employe.py

--- employe.py
+++ employe.py
@@ -1,14 +1,14 @@
 def employee_details(name, emp_id, department, salary):
     result = (
-        f"name: {name}\n"
-        f"emp_id: {emp_id}\n"
-        f"department{department}\n"
-        f"salary: {salary}"
+        f"name:{name}\n"
+        f"emp_id:{emp_id}\n"
+        f"department:{department}\n"
+        f"salary:{salary}"
     )
     return result
 if __name__ == "__main__":
-    name = "furkhan"
-    emp_id = "E1001"
-    department = "kitchen"
-    salary = 250
+    name ="furkhan"
+    emp_id ="E1001"
+    department ="kitchen"
+    salary =250
     print(employee_details(name, emp_id, department, salary))
